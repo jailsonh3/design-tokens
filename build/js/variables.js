98adc2d68300c3fed2bbf13030890c006042283b
jailsonh3 <jailsonh3@gmail.com>
2022-10-09 13:38:04 -0300

    Initial commit

---
+++ build/js/variables.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 09 Oct 2022 16:22:11 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray400": "#a3a3a3",
+  "ColorGray800": "#262626",
+  "ColorGray900": "#171717",
+  "ColorPurple500": "#8b5cf6",
+  "EffectShadowMd": {"shadowType":"dropShadow","radius":4,"color":"#ffffff33","offsetX":0,"offsetY":2,"spread":0}
+};
